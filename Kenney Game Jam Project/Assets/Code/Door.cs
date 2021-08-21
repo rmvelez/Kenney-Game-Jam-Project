7b08f98d54fb9692e87ac155a3214b8af6d31372
David Henshaw <davidhenshaw2014@gmail.com>
2021-08-21 09:17:00 -0500

    added exit door

---
+++ Kenney Game Jam Project/Assets/Code/Door.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public static event Action touchedPlayer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMovement player = collision.GetComponent<PlayerMovement>();
+
+        if(player)
+        {
+            touchedPlayer?.Invoke();
+        }
+    }
+}
---
+++ Kenney Game Jam Project/Assets/Code/GameState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    private static GameState instance;
+    public GameState Instance => instance;
+
+    public event Action levelCompleted;
+    public event Action loadNextLevel;
+
+    bool levelComplete = false;
+
+    private void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+            SubscribeEvents();
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void SubscribeEvents()
+    {
+        Door.touchedPlayer += OnDoorTouched;
+    }
+
+    private void OnDoorTouched()
+    {
+        levelComplete = true;
+        Debug.Log("Player has reached the door");
+    }
+
+}
