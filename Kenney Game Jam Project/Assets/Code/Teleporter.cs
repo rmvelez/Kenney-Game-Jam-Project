5dfc6c62b703475adc49959edebfed5a95529e8a
David Henshaw <davidhenshaw2014@gmail.com>
2021-08-22 00:58:49 -0500

    teleportation complete

---
+++ Kenney Game Jam Project/Assets/Code/Teleporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Teleporter other;
+    bool isActive = true;
+
+    public void OnRecieveTeleport()
+    {
+        isActive = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isActive)
+            return;
+
+        other.OnRecieveTeleport();
+        collision.transform.position = other.transform.position;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isActive = true;
+    }
+}
