cb2ae26a216789666556ff824d2726105fdd298e
rmvelez <ricardo.velez@quinnipiac.edu>
2021-08-22 10:07:59 -0400

    UI Update
    Changed the Text and Images for buttons. Also Added code for both the title screen and the win scene.

---
+++ Kenney Game Jam Project/Assets/Code/Rick Velez/WinControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
